0e770f3a4744d58168261887feafcdef085cb1d2
narumi <toucans-cutouts0f@icloud.com>
2025-05-17 21:19:07 +0800

    sort rate by spot spread

--- src/twrate/cli.py
+++ src/twrate/cli.py
@@ -6,6 +6,7 @@ from .bot import query_bot_rates
 from .dbs import query_dbs_rates
 from .esun import query_esun_rates
 from .sinopac import query_sinopac_rates
+from .types import Rate
 
 
 def run(source_currency: str) -> None:
@@ -17,6 +18,15 @@ def run(source_currency: str) -> None:
     table = []
 
     rates = query_bot_rates() + query_dbs_rates() + query_sinopac_rates() + query_esun_rates()
+
+    # sort rates by spot_spread
+    def sort_key(rate: Rate) -> float:
+        if rate.spot_spread is None:
+            return float("inf")
+        return rate.spot_spread
+
+    rates = sorted(rates, key=sort_key)
+
     for rate in rates:
         if rate.source == source_currency.upper():
             table.append(
@@ -24,11 +34,27 @@ def run(source_currency: str) -> None:
                     rate.exchange,
                     rate.spot_buy,
                     rate.spot_sell,
+                    f"{rate.spot_spread * 100:.2f}%" if rate.spot_spread is not None else None,
                     rate.cash_buy,
                     rate.cash_sell,
+                    f"{rate.cash_spread * 100:.2f}%" if rate.cash_spread is not None else None,
                 ]
             )
-    print(tabulate(table, headers=["Exchange", "Spot Buy", "Spot Sell", "Cash Buy", "Cash Sell"]))
+
+    print(
+        tabulate(
+            table,
+            headers=[
+                "Exchange",
+                "Spot Buy",
+                "Spot Sell",
+                "Spot Spread",
+                "Cash Buy",
+                "Cash Sell",
+                "Cash Spread",
+            ],
+        )
+    )
 
 
 def main() -> None:
